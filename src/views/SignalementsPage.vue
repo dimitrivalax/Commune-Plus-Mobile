d481709e93ceb83db9f3fd9c119ec05f1b3816c6
Dimitri Valax <dimitri.valax@beta.gouv.fr>
2026-01-25 18:42:23 +0100

    bugfix : refresher below select

--- src/views/SignalementsPage.vue
+++ src/views/SignalementsPage.vue
@@ -8,16 +8,22 @@
         </IonFabButton>
       </IonFab>
 
+      <IonRefresher
+        slot="fixed"
+        @ionRefresh="loadSignalements($event)"
+        class=""
+      >
+        <IonRefresherContent></IonRefresherContent>
+      </IonRefresher>
       <div class="ion-padding">
-        <IonRefresher slot="fixed" @ionRefresh="loadSignalements($event)">
-          <IonRefresherContent></IonRefresherContent>
-        </IonRefresher>
-
         <!-- Filtre par statut -->
-        <IonItem class="filter-item"
-        lines="none">
+        <IonItem class="filter-item" lines="none">
           <IonLabel>Filtrer par statut</IonLabel>
-          <IonSelect v-model="selectedStatus" placeholder="Tous les statuts" interface="popover">
+          <IonSelect
+            v-model="selectedStatus"
+            placeholder="Tous les statuts"
+            interface="popover"
+          >
             <IonSelectOption value="all">Tous</IonSelectOption>
             <IonSelectOption value="en_attente">En Attente</IonSelectOption>
             <IonSelectOption value="en_cours">En cours</IonSelectOption>
@@ -26,11 +32,23 @@
           </IonSelect>
         </IonItem>
 
-        <IonList v-if="filteredSignalements.length > 0" class="signalements-list">
-          <IonItem v-for="signalement in filteredSignalements" :key="signalement.id" class="signalement-item" button lines="none"
-            @click="$router.push(`/signalement/${signalement.id}`)">
+        <IonList
+          v-if="filteredSignalements.length > 0"
+          class="signalements-list"
+        >
+          <IonItem
+            v-for="signalement in filteredSignalements"
+            :key="signalement.id"
+            class="signalement-item"
+            button
+            lines="none"
+            @click="$router.push(`/signalement/${signalement.id}`)"
+          >
             <IonThumbnail v-if="signalement.photo_url" slot="start">
-              <img :src="signalement.photo_url" :alt="signalement.description" />
+              <img
+                :src="signalement.photo_url"
+                :alt="signalement.description"
+              />
             </IonThumbnail>
             <IonLabel>
               <h2>{{ signalement.description || 'Sans description' }}</h2>
@@ -41,7 +59,10 @@
                 <span class="date-text">{{
                   formatDateTime(signalement.created_at)
                 }}</span>
-                <IonBadge :color="getStatusColor(signalement.status)" class="status-badge">
+                <IonBadge
+                  :color="getStatusColor(signalement.status)"
+                  class="status-badge"
+                >
                   {{ getStatusLabel(signalement.status) }}
                 </IonBadge>
               </div>
@@ -52,10 +73,16 @@
         <div v-else class="empty-state">
           <IonIcon :icon="documentText" class="empty-icon" />
           <h3>Aucun signalement</h3>
-          <p v-if="selectedStatus === 'all'">Aucun signalement pour le moment.</p>
+          <p v-if="selectedStatus === 'all'">
+            Aucun signalement pour le moment.
+          </p>
           <p v-else>Aucun signalement avec ce statut.</p>
-          <IonButton v-if="selectedStatus === 'all'" expand="block" @click="$router.push('/signalement/new')"
-            class="empty-action">
+          <IonButton
+            v-if="selectedStatus === 'all'"
+            expand="block"
+            @click="$router.push('/signalement/new')"
+            class="empty-action"
+          >
             Faire un signalement
           </IonButton>
         </div>
@@ -81,7 +108,7 @@ import {
   IonRefresher,
   IonRefresherContent,
   IonSelect,
-  IonSelectOption,
+  IonSelectOption
 } from '@ionic/vue'
 import { add, documentText } from 'ionicons/icons'
 import AppHeader from '@/components/AppHeader.vue'
