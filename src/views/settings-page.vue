b1b829a9d26450c00abcbe0d9218e1a79ab3661c
Dimitri Valax <dimitri.valax@beta.gouv.fr>
2026-02-20 16:50:11 +0100

    feat: Update Android versionCode from 11 to 12 for versioning consistency and change app terminology from "Actualitéss de la commune" to "Informations de la commune" in settings page for clarity.

--- src/views/settings-page.vue
+++ src/views/settings-page.vue
@@ -91,7 +91,7 @@
         <div class="settings-section">
           <h2 class="section-title">
             <ion-icon :icon="location" />
-            Actualitéss de la commune
+            Informations de la commune
           </h2>
           <ion-card>
             <ion-card-content>
